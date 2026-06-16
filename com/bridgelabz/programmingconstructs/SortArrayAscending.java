c6e5933f01b92bdc64c5a9f03c6372c7560621e5
R Navya <ravagunta.navya@gmail.com>
2026-06-16 17:23:29 +0530

    [Navya]: Sort array in ascending order

---
+++ com/bridgelabz/programmingconstructs/SortArrayAscending.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.programmingconstructs;
+
+import java.util.Scanner;
+
+public class SortArrayAscending {
+
+    public static void sortByAsc(int arr[]) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] > arr[j]) {
+
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Array in Ascending Order:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sortByAsc(arr);
+    }
+}
