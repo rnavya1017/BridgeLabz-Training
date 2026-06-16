a06a4bb756a373c07efb912f46a628a061eb336a
R Navya <ravagunta.navya@gmail.com>
2026-06-16 19:45:05 +0530

    [Navya]: frequency elements in array

---
+++ com/bridgelabz/programmingconstructs/FrequencyElementsArray.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.programmingconstructs;
+
+import java.util.Scanner;
+
+public class FrequencyElementsArray {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            boolean visited[] = new boolean[n];
+
+            for(int i=0;i<n;i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            for(int i=0;i<n;i++) {
+                if(visited[i])
+                    continue;
+
+                int count = 1;
+
+                for(int j=i+1;j<n;j++) {
+                    if(arr[i] == arr[j]) {
+                        count++;
+                        visited[j] = true;
+                    }
+                }
+
+                System.out.println(arr[i] + " -> " + count);
+            }
+        }
+    }
+
