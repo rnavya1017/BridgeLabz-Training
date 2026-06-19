bb75286f80f3e46c6580efa3f9019101481ca20b
R Navya <ravagunta.navya@gmail.com>
2026-06-19 11:21:06 +0530

    [Navya]:Stop watch

---
+++ com/bridgelabz/programmingconstructs/StopWatch.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.programmingconstructs;
+
+import java.util.Scanner;
+
+public class StopWatch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Press Enter to Start Stopwatch");
+        sc.nextLine();
+
+        long startTime = System.currentTimeMillis();
+
+        System.out.println("Press Enter to Stop Stopwatch");
+        sc.nextLine();
+
+        long endTime = System.currentTimeMillis();
+
+        long elapsedTime = endTime - startTime;
+
+        System.out.println("Elapsed Time: " + elapsedTime + " milliseconds");
+        System.out.println("Elapsed Time: " + (elapsedTime / 1000.0) + " seconds");
+
+
+    }
+}
