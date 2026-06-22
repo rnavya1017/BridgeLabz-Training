77b1c944a6c4ac3afb3bb8c48a9d9d54d53c45c8
R Navya <ravagunta.navya@gmail.com>
2026-06-22 12:00:21 +0530

    [navya]:commandline arguments sum

---
+++ com/bridgelabz/datatypes/SumArguments.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.datatypes;
+
+public class SumArguments {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int invalidCount = 0;
+
+        for (String arg : args) {
+            try {
+                int num = Integer.parseInt(arg);
+                sum += num;
+            } catch (Exception e) {
+                invalidCount++;
+            }
+        }
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Invalid Integers = " + invalidCount);
+    }
+}
