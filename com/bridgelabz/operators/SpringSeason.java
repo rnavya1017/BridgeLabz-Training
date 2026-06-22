77e2c841be7ca55bd18aa678bdac2b00cb915f27
R Navya <ravagunta.navya@gmail.com>
2026-06-22 12:47:16 +0530

    [Navya]:find spring season of which month

---
+++ com/bridgelabz/operators/SpringSeason.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.operators;
+
+public class SpringSeason {
+
+    public static void main(String[] args) {
+
+        int m = Integer.parseInt(args[0]);
+        int d = Integer.parseInt(args[1]);
+
+        boolean spring =
+                (m == 3 && d >= 20) ||
+                        (m == 4) ||
+                        (m == 5) ||
+                        (m == 6 && d <= 20);
+
+        System.out.println(spring);
+    }
+}
